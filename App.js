8664020d26bc2dec7f313e9ec40353eb0ccaee3e
tanayshah454 <68338994+tanayshah454@users.noreply.github.com>
2021-06-17 10:12:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen'
+
+class App extends Component {
+
+  render() {
+    return (
+      <WelcomeScreen/>
+    );
+  }
+}
+
+export default App;
